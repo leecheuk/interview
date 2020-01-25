c9e3f3f722049253cbdbe5f638c0cce711d375e8
m <dev.cheuk.lee@gmail.com>
2020-01-25 11:40:58 -0500

    set api version

--- web_app/index.js
+++ web_app/index.js
@@ -7,6 +7,7 @@ const PORT = 3002;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/api", (req, res) => {
+    res.set('Api-Version', '1.0.0');
     res.status(200).json({ data: "data" });
 });
 
@@ -14,4 +15,6 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 
-app.listen(PORT);
+app.listen(PORT, () => {
+    console.log(`App started: http://localhost:${PORT}`);
+});
